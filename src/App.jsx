99c6c1e0135b8cf933c7ab8df749ed482810af5b
ApurvaChoudhary45 <badassmannu45@gmail.com>
2025-04-26 03:26:51 +0530

    Oevrflow Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
+import './index.css'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import About from './components/About'
@@ -14,9 +15,9 @@ function App() {
 
   return (
     <>
-    <div className='overflox-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 slecetion:text-cyan-900'>
+    <div className='overflox-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
       <div className='fixed top-0 -z-10 h-full w-full'>
-      <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>
+      <div className="relative h-full w-full bg-slate-950"><div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>
       </div>
      
       
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ import { FaSquareTwitter } from 'react-icons/fa6'
 import logo from "../assets/Logo.png"
 const Navbar = () => {
   return (
-    <nav className=' mb-20 flex items-center justify-between y-6'>
+    <nav className=' mb-20 flex items-center justify-between py-6'>
         <div className='flex flex-shrink-0 items-center'>
-            <img src={logo} alt="" className='w-15 mx-2 rounded-full' />
+            <img src={logo} alt="" className='w-12 mx-2 rounded-full' />
         </div>
         <div className='m-8 flex items-center justify-center gap-5 text-2xl'>
             <a href='https://www.linkedin.com/in/apurva-singh-choudhary-374991223' style={{cursor: 'pointer'}}><FaLinkedin/></a>
